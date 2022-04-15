7d0d4c7991c236293d64666fa573ab1a1b103fb9
Ari Budi Prasetyo <54903849+arityo182@users.noreply.github.com>
2022-04-15 10:08:34 +0700

    Add files via upload

---
+++ db_anaksekolah.sql
@@ -0,0 +1,51 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Apr 15, 2022 at 05:06 AM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 7.4.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_anaksekolah`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `guru`
+--
+-- Error reading structure for table db_anaksekolah.guru: #1932 - Table 'db_anaksekolah.guru' doesn't exist in engine
+-- Error reading data for table db_anaksekolah.guru: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `db_anaksekolah`.`guru`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `murid`
+--
+-- Error reading structure for table db_anaksekolah.murid: #1932 - Table 'db_anaksekolah.murid' doesn't exist in engine
+-- Error reading data for table db_anaksekolah.murid: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `db_anaksekolah`.`murid`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+-- Error reading structure for table db_anaksekolah.users: #1932 - Table 'db_anaksekolah.users' doesn't exist in engine
+-- Error reading data for table db_anaksekolah.users: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `db_anaksekolah`.`users`' at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
